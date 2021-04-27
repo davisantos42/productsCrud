9f8bd708183ec557c1c88a0cfe41d894b1514aeb
davisantos42 <davisantos@casinocopa.com.br>
2021-04-27 16:39:13 -0300

    delete.php improved, uses GET instead of POST

--- delete.php
+++ delete.php
@@ -2,9 +2,15 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', 'Qazscde440*');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$productId = $_GET['id'];
+$productId = $_POST['id'] ?? null;
 
-$statement = $pdo->prepare("DELETE FROM products WHERE id=$productId");
+if (!$productId) {
+    header('Location: index.php');
+}
+
+$statement = $pdo->prepare("DELETE FROM products WHERE id = :id");
+$statement->bindValue(':id', $productId);
+
 $statement->execute();
 
 header('Location: index.php');
--- index.php
+++ index.php
@@ -51,7 +51,11 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
                     <td><?php echo $product['create_date']; ?></td>
                     <td>
                         <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
-                        <a href="delete.php?id=<?php echo $product['id'] ?>" type="button" class="btn btn-sm btn-outline-danger">Delete</a>
+                        <form style="display: inline-block;" action="./delete.php" method="post">
+                            <input type="hidden" name="id" value="<?php echo $product['id'] ?>">
+                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                        </form>
+
                     </td>
                 </tr>
             <?php endforeach; ?>
